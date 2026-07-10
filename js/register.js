f8bb3f4d9cf96e58469da73af6f65dac6f9099c1
vishnu5495 <minitoon5495@gmail.com>
2026-07-10 16:57:54 +0530

    Implement user registration with Firebase
    Added user registration functionality with Firebase authentication and Firestore database integration.

--- js/register.js
+++ js/register.js
@@ -1,1 +1,56 @@
+import { auth, db } from "./firebase-config.js";
 
+import {
+  createUserWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+
+import {
+  doc,
+  setDoc
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+
+const registerForm = document.getElementById("registerForm");
+
+registerForm.addEventListener("submit", async (e) => {
+
+  e.preventDefault();
+
+  const fullname = document.getElementById("fullname").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const mobile = document.getElementById("mobile").value.trim();
+  const password = document.getElementById("password").value;
+  const confirmPassword = document.getElementById("confirm_password").value;
+
+  if (password !== confirmPassword) {
+    alert("Password and Confirm Password do not match.");
+    return;
+  }
+
+  try {
+
+    const userCredential =
+      await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+    const user = userCredential.user;
+    await setDoc(doc(db, "customers", user.uid), {
+      fullName: fullname,
+      email: email,
+      mobile: mobile,
+      createdAt: new Date().toISOString()
+    });
+
+    alert("Registration Successful!");
+
+    window.location.href = "login.html";
+
+  } catch (error) {
+
+    alert(error.message);
+
+  }
+
+});
