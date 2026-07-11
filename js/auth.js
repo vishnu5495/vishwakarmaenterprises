a493ce8f0703cc9f514b506aa66aaa68f19dc419
vishnu5495 <minitoon5495@gmail.com>
2026-07-11 07:49:47 +0530

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,27 @@
+import { auth } from "./firebase-config.js";
 
+import {
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+
+onAuthStateChanged(auth, (user) => {
+
+  const page = window.location.pathname.split("/").pop();
+
+  // Login और Register page हमेशा खुलेंगे
+  if (page === "login.html" || page === "register.html") {
+
+    // अगर पहले से Login है तो Home भेज दो
+    if (user) {
+      window.location.href = "index.html";
+    }
+
+    return;
+  }
+
+  // बाकी सभी Pages पर Login जरूरी है
+  if (!user) {
+    window.location.href = "login.html";
+  }
+
+});
